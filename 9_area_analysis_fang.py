2c07893a3717e973399a3fcd2c994299553c0b2a
yumang1cv <xuyang_cv@foxmail.com>
2022-04-27 16:31:37 +0800

    20220427

--- 9_area_analysis_fang.py
+++ 9_area_analysis_fang.py
@@ -57,55 +57,6 @@ def choose_data(dataframe, column="", element=""):
     return df
 
 
-def rotate_around_point_highperf(point, radians, origin=(0, 0)):
-    """Rotate a point around a given point.
-
-    I call this the "high performance" version since we're caching some
-    values that are needed >1 time. It's less readable than the previous
-    function but it's faster.
-    """
-    radians = radians / 180 * math.pi
-    x_1, y_1 = point
-    offset_x, offset_y = origin
-    adjusted_x = (x_1 - offset_x)
-    adjusted_y = (y_1 - offset_y)
-    cos_rad = math.cos(radians)
-    sin_rad = math.sin(radians)
-    qx = offset_x + cos_rad * adjusted_x + sin_rad * adjusted_y
-    qy = offset_y + -sin_rad * adjusted_x + cos_rad * adjusted_y
-
-    return qx, qy
-
-
-def rotation(dataframe):
-    df1 = dataframe.iloc[2:, 5:7]  # select back vector
-    df1 = df1.astype(float)
-    # df1['back'], df1['back.1'] = rotate_around_point_highperf(df1, 0.3, origin=(0, 0))
-    x_max = df1['x'].max()
-    # print(df1[df1['back'] == x_max].index.values)
-    y1 = df1['y'][df1[df1['x'] == x_max].index.values]
-    y1 = y1.values[0]
-    x_min = df1['x'].min()
-    y_max = df1['y'].max()
-    y_min = df1['y'].min()
-    x1 = df1['x'][df1[df1['y'] == y_min].index.values]
-    x1 = x1.values[0]
-
-    L1 = y1 - y_min
-    L2 = x_max - x1
-    theta = math.atan(L1 / L2) / math.pi * 180
-
-    x_mean = df1['x'].mean()
-    y_mean = df1['y'].mean()
-
-    df2 = df1.copy()
-    for i in range(len(df1)):
-        point = [df1['x'].iloc[i], df1['y'].iloc[i]]
-        df2['x'].iloc[i], df2['y'].iloc[i] = rotate_around_point_highperf(point, theta, origin=(0, 0))
-
-    return df2
-
-
 def normliza_data(data1, data_name=''):
     size = int(len(data1) / 2)
     norm_data = np.array(data1[data_name]).reshape(size, 2)
@@ -118,25 +69,56 @@ def normliza_data(data1, data_name=''):
     return norm_data
 
 
-if __name__ == '__main__':
+def nine_area_analysis(file_list, length):  # 传统 9 区域分析   边长按照 1 : 2 : 1 划分
 
-    # file_path = []
-    # path = r'D:/3D_behavior/Spontaneous_behavior/result_fang/3Dskeleton/Calibrated_3DSkeleton'
-    # for root, directories, file in os.walk(path):
-    #     for file in file:
-    #         if file.endswith(".csv"):
-    #             # print(os.path.join(root, file))
-    #             file_path.append(os.path.join(root, file))
+    boundary = length / 4
+    angle_time = []
+    center_time = []
+    line_time = []
+
+    for file_num in range(len(csv_FD)):
+
+        data1 = pd.read_csv(csv_FD[file_num])
+        data2 = data1.iloc[2:, 4:7]
+
+        x = 0  # 四角时间
+        y = 0  # 边界时间
+        z = 0  # 中心时间
+        for i in range(len(data2)):
+            if (data2['x'].iloc[i] >= boundary and data2['y'].iloc[i] >= boundary) or \
+                    (data2['x'].iloc[i] <= -boundary and data2['y'].iloc[i] <= -boundary) or \
+                    (data2['x'].iloc[i] <= -boundary and data2['y'].iloc[i] >= boundary) or \
+                    (data2['x'].iloc[i] >= boundary and data2['y'].iloc[i] <= -boundary):
+                x = x + 1
+
+            elif (-boundary <= data2['x'].iloc[i] <= boundary <= data2['y'].iloc[i]) or \
+                    (boundary >= data2['x'].iloc[i] >= -boundary >= data2['y'].iloc[i]) or \
+                    (-boundary <= data2['y'].iloc[i] <= boundary <= data2['x'].iloc[i]) or \
+                    (boundary >= data2['y'].iloc[i] >= -boundary >= data2['x'].iloc[i]):
+                y = y + 1
+
+            # elif np.abs(data2['x'].iloc[i]) <= 125 and np.abs(data2['y'].iloc[i]) <= 125:
+            else:
+                z = z + 1
+
+        angle_time.append(x / 30)
+        line_time.append(y / 30)
+        center_time.append(z / 30)
+
+    return angle_time, center_time, line_time
+
+
+if __name__ == '__main__':
 
     a = read_csv(path=r'D:/3D_behavior/Spontaneous_behavior/result_fang',
                  name="video_info.xlsx", column='roundTime', element=1)
 
     A = choose_data(a, column='ExperimentTime', element='day')
     B = choose_data(A, column='gender', element='male')
-    all_time = []
     angle_time_all = []
-    circle_time_all = []
-    for time_state in range(1, 2):
+    center_time_all = []
+    line_time_all = []
+    for time_state in range(1, 7):
         # time_state = 1
         # 多条件筛选
         X = choose_data(a, column='split_number', element=time_state)  # split_number=1 not have ''
@@ -148,4 +130,57 @@ if __name__ == '__main__':
                 path=r"D:/3D_behavior/Spontaneous_behavior/result_fang/3Dskeleton/back_coordinates_origin/",
                 name="rec-{}-G1-2022114230_Cali_Data3d_Replace".format(item))
             csv_FD.append(csv_result3[0])
+
+        angle_time_single, center_time_single, line_times_single = nine_area_analysis(csv_FD, length=500)
+
+        angle_time_all.append(angle_time_single)
+        center_time_all.append(center_time_single)
+        line_time_all.append(line_times_single)
+
+        print('第{}分钟已处理结束'.format(time_state*10))
+
+
+
+
+
+
+
+
+
+    """
+        传统 9 区域分析   边长按照 1 : 2 : 1 划分
+    """
+    # angle_time = []
+    # center_time = []
+    # line_time = []
+    #
+    # for file_num in range(len(csv_FD)):
+    #
+    #     data1 = pd.read_csv(csv_FD[file_num])
+    #     data2 = data1.iloc[2:, 4:7]
+    #
+    #     x = 0  # 四角时间
+    #     y = 0  # 边界时间
+    #     z = 0  # 中心时间
+    #     for i in range(len(data2)):
+    #         if (data2['x'].iloc[i] >= 125 and data2['y'].iloc[i] >= 125) or \
+    #                 (data2['x'].iloc[i] <= -125 and data2['y'].iloc[i] <= -125) or \
+    #                 (data2['x'].iloc[i] <= -125 and data2['y'].iloc[i] >= 125) or \
+    #                 (data2['x'].iloc[i] >= 125 and data2['y'].iloc[i] <= -125):
+    #             x = x + 1
+    #
+    #         elif (125 >= data2['x'].iloc[i] >= -125 and data2['y'].iloc[i] >= 125) or \
+    #                 (125 >= data2['x'].iloc[i] >= -125 >= data2['y'].iloc[i]) or \
+    #                 (125 >= data2['y'].iloc[i] >= -125 and data2['x'].iloc[i] >= 125) or \
+    #                 (125 >= data2['y'].iloc[i] >= -125 >= data2['x'].iloc[i]):
+    #             y = y + 1
+    #
+    #         # elif np.abs(data2['x'].iloc[i]) <= 125 and np.abs(data2['y'].iloc[i]) <= 125:
+    #         else:
+    #             z = z + 1
+    #
+    #     angle_time.append(x / 30)
+    #     center_time.append(y / 30)
+    #     line_time.append(z / 30)
+
 
